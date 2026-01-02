57213a3f04335c6e4f42a1f02a2ee63f44e8d92b
Iq mongol <battsengel4567@gmail.com>
2026-01-02 08:17:29 +0000

    update

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function closeModal(id) {
     document.getElementById(id).style.display = "none";
 }
 
-// Хайлтын логик
+// Хайлтын систем
 function searchMehs() {
     let input = document.getElementById('searchInput').value.toLowerCase();
     let cards = document.getElementsByClassName('card');
 
     for (let i = 0; i < cards.length; i++) {
-        let title = cards[i].getAttribute('data-title');
-        if (title.includes(input)) {
+        let name = cards[i].getAttribute('data-name');
+        if (name.includes(input)) {
             cards[i].style.display = "block";
         } else {
             cards[i].style.display = "none";
@@ -21,7 +21,7 @@ function searchMehs() {
     }
 }
 
-// Гадуур дарахад хаагдах
+// Гадна талд дарахад хаагдах
 window.onclick = function(event) {
     if (event.target.className === 'modal') {
         event.target.style.display = "none";
