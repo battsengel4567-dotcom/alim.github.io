7a24cedac09cc8e894a29fd19196fd5a71b798f1
Iq mongol <battsengel4567@gmail.com>
2026-01-02 08:09:23 +0000

    update

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ window.onclick = function(event) {
         event.target.style.display = "none";
     }
 }
+function openModal(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+// ХАЙЛТЫН ФУНКЦ
+function searchMehs() {
+    let input = document.getElementById('searchInput').value.toLowerCase();
+    let cards = document.getElementsByClassName('card');
+
+    for (let i = 0; i < cards.length; i++) {
+        let title = cards[i].getAttribute('data-title');
+        if (title.includes(input)) {
+            cards[i].style.display = "block";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+}
+
+// Гадна талд дарахад хаагдах
+window.onclick = function(event) {
+    if (event.target.className === 'modal') {
+        event.target.style.display = "none";
+    }
+}
